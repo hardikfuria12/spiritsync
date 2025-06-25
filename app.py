7795bc04855b40fd1e09b686c3cd6c20e4f3634d
Hardik Furia <hardikfuria12@gmail.com>
2025-06-25 20:22:57 +0530

    simple upload page

--- app.py
+++ app.py
@@ -2,7 +2,21 @@ from flask import Flask, render_template, request, redirect, url_for
 import os
 
 app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)\
+
+
+@app.route('/upload',methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(filepath)
+            return f"File uploaded successfully: {file.filename}"
+    return render_template('upload.html')
 @app.route('/')
 def index():
     return render_template('index.html')
