9443cd7c0d551a7c2c7dc457c85e85a1eaaff629
Hardik Furia <hardikfuria12@gmail.com>
2025-07-08 23:07:15 +0530

    hopeful

--- app.py
+++ app.py
@@ -43,7 +43,12 @@ def submit():
             return f"Backend error: {response.text}", 500
 
         data = response.json()
-        return render_template("result.html", user_id=data['user_id'], table_html=data['table_html'], NGROK_BACKEND_URL=NGROK_BACKEND_URL)
+        return render_template(
+            "result.html",
+            user_id=data['user_id'],
+            table_html=data['table_html'],
+            NGROK_BACKEND_URL=NGROK_BACKEND_URL
+        )
 
 
     except requests.exceptions.RequestException as e:
