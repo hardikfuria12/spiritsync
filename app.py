be91b85b6de0e1c9fb52e1778e952fca16596801
Hardik Furia <hardikfuria12@gmail.com>
2025-07-29 00:30:54 +0530

    port change for work

--- app.py
+++ app.py
@@ -85,5 +85,5 @@ def save_family():
         return f"Server error: {e}", 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5001))
     app.run(debug=True, host='0.0.0.0', port=port)
